ef4d4a98360a6bdc112523316ae9f4966cc44115
Shakia <harperh282@gmail.com>
2022-06-24 19:56:42 -0500

    Star Trek Client-Side

---
+++ main.js
@@ -0,0 +1,19 @@
+document.querySelector('#getButton').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const name = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://star-trek-api-alien-search.herokuapp.com/api/${name}`)
+        const data =  await response.json()
+        console.log(data)
+        document.getElementById('alienName').innerText = data.speciesName
+        document.getElementById('alienWorld').innerText = data.homeworld
+        document.getElementById('alienFeatures').innerText = data.features
+        document.getElementById('alienFacts').innerText = data.interestingFact
+        document.getElementById('alienExamples').innerText = data.notableExamples
+        document.getElementById('alienImage').src = data.image
+        document.getElementById('alienCaption').innerText = data.speciesName
+    } catch{
+        console.log(error)
+    }
+}
