751a9791a9fa837fa24a9acc40d774c2580ff13c
Shakia <harperh282@gmail.com>
2022-12-02 12:58:45 -0600

    updated fetch

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.querySelector('#getButton').addEventListener('click', apiRequest)
 async function apiRequest(){
     const name = document.querySelector('input').value
     try{
-        const response = await fetch(`https://star-trek-api-alien-search.herokuapp.com/api/${name}`)
+        const response = await fetch(`https://star-trek-api.onrender.com/api/${name}`)
         const data =  await response.json()
         console.log(data)
         document.getElementById('alienName').innerText = data.speciesName
